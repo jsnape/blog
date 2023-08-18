8dee97f0d19ac1d4e0318bd704649f7b40df5c84
James Snape <james.snape@hitachivantara.com>
2023-08-18 16:41:28 +0100

    Add tags pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,19 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    markdown: {
+      render: [
+        '@astrojs/markdown-remark',
+        {
+          syntaxHighlight: 'shiki',
+          shikiConfig: {
+            theme: 'nord',
+            langs: ['js', 'html', 'css', 'astro', "csharp"],
+            wrap: false,
+          },
+        },
+      ],
+    },
+  });
+  
--- src/pages/posts/post-3.md
+++ src/pages/posts/post-3.md
@@ -10,3 +10,15 @@ pubDate: 2022-07-15
 tags: ["astro", "learning in public", "setbacks", "community"]
 ---
 It wasn't always smooth sailing, but I'm enjoying building with Astro. And, the [Discord community](https://astro.build/chat) is really friendly and helpful!
+
+```csharp
+namespace MyProject;
+
+public class Foo
+{
+    public Foo()
+    {
+    }
+}
+
+`````
