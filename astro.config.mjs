8068ae548d92fc3fd43bad0dad6b9360a84dee94
James Snape <james.snape@hitachivantara.com>
2023-08-18 17:43:05 +0100

    Add island and dark theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,18 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    markdown: {
-      render: [
-        '@astrojs/markdown-remark',
-        {
-          syntaxHighlight: 'shiki',
-          shikiConfig: {
-            theme: 'nord',
-            langs: ['js', 'html', 'css', 'astro', "csharp"],
-            wrap: false,
-          },
-        },
-      ],
-    },
-  });
-  
+  markdown: {
+    render: ['@astrojs/markdown-remark', {
+      syntaxHighlight: 'shiki',
+      shikiConfig: {
+        theme: 'nord',
+        langs: ['js', 'html', 'css', 'astro', "csharp"],
+        wrap: false
+      }
+    }]
+  },
+  integrations: [preact()]
+});
